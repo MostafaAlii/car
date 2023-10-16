8ee3c6bfb6e1a4bfe517a3293763c72b9b7f47a6
Mostafa Ali <57988145+MostafaAlii@users.noreply.github.com>
2023-10-16 16:13:41 +0300

    Update reject.blade.php

--- resources/views/dashboard/admin/captains/btn/modals/profile/reject.blade.php
+++ resources/views/dashboard/admin/captains/btn/modals/profile/reject.blade.php
@@ -3,7 +3,7 @@
     <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="carRejectModalLabel">{{ ucfirst(str_replace('_', ' ', $image->photo_type)) }} Active</h5>
+                <h5 class="modal-title" id="carRejectModalLabel">{{ ucfirst(str_replace('_', ' ', $image->photo_type)) }} Reject</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
